fa29570fee52734a7e66abbf6aca2148836aaec3
尹畅 <yinchang@yinchangdemacbook-pro.local>
2021-03-18 12:29:07 +0800

    Init Blog

--- blog.config.js
+++ blog.config.js
@@ -1,20 +1,20 @@
 const BLOG = {
-  title: 'Ian Blog',
-  author: 'Ian',
-  email: '1144028227@qq.com',
-  link: 'https://nobelium.vercel.app',
-  description: '产品狗',
-  lang: 'zh',
+  title: 'CRAIGARY', // 标题
+  author: 'Craig Hart', // 作者
+  email: 'i@craigary.net', // 邮箱
+  link: 'https://nobelium.vercel.app', // 填入域名
+  description: 'This gonna be an awesome website.', // 博客描述
+  lang: 'en-US',
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // if leave this empty, current year will be used.
+  since: 2021, // 建站年份
   postsPerPage: 7,
   showAbout: true, // WIP
   showArchive: true, // WIP
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/craigaryhart', // 社交链接
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
-  },
+  }, 
   notionPageId: process.env.NOTION_PAGE_ID,
   comment: { // support provider: gitalk
     provider: '', // leave it empty if you don't need any comment plugin
